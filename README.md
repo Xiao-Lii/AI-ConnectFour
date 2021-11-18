a5db098e249b0e3832acb803461df2672484378b
Xiao-Lii <63008453+Xiao-Lii@users.noreply.github.com>
2021-11-18 11:47:06 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# AI-ConnectFour
+ As long as this AI starts as player 1, it will always win no matter what move or strategy player 2 makes.
---
+++ connectFour.py
@@ -0,0 +1,35 @@
+import json
+import sys
+import random
+
+# We'll be utilizing dumps(dump string) & loads(load string)
+# Python enumerates up to n - 1 so we don't need to subtract by 1
+def valid_moves(precept):
+    grid = precept['grid']
+    moves = []  # Can't initialize w/ Int 64s
+    for i, col in enumerate(grid):
+        if col[0] == 0:
+            moves.append(i)
+    # This is the same as lines 10 - 12 that constructs an array 
+    # With a condition
+    #moves = [i for i, col in enumerate(grid) if col[0] == 0]
+    return moves
+
+def main():
+    print("Connect Four in Python", file=sys.stderr)
+
+    # This next line of code will have an intentional mistake
+    #for json in sys.stdin:  # Misinterprets json class vs object 
+    for line in sys.stdin:  
+        print(line, file=sys.stderr)
+        precept = json.loads(line)      # This loads precept as a dict
+        moves = valid_moves(precept)    # Returns a list of all valid moves
+        move = random.choice(moves) 
+        action = {'move': move}
+        action_json = json.dumps(action)
+        print(action_json, file=sys.stderr)
+        print(action_json, flush=True)  # Stdout is default for python 
+        
+
+if __name__ == '__main__':
+    main()

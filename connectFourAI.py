e2bf097c777a26f1fd96711af7ab03e0156a7c8d
Xiao-Lii <mellowjellolee@gmail.com>
2021-11-19 21:52:22 -0700

    resolved some issues with minimax function

--- connectFourAI.py
+++ connectFourAI.py
@@ -2,7 +2,9 @@ import json
 import sys
 import random
 import math
+from typing import get_args
 import numpy as np
+
 
 PLAYER = 2
 AI = 1
@@ -11,30 +13,36 @@ AI_PIECE = 1
 PLAYER_PIECE = 2
 WINDOW_LENGTH = 4
 
+
 def get_board(game_dict):
     grid = game_dict['grid']        # Save the 'grid' key from our dict read from the json obj
     return grid
 
-def get_next_open_row(board, col, height):
+
+def find_next_row(board, col, height):
 	for row in range(height):
 		if board[row][col] == 0:
 			return row
+
 
 def get_depth(game_dict):
     depth = game_dict['height']
     return depth
 
+
 def get_width(game_dict):
     width = game_dict['width']
     return width    
+
 
 def get_player(game_dict):
     player = game_dict['player']    
     return player
 
+
 # We'll be utilizing dumps(dump string) & loads(load string)
 # Python enumerates up to n - 1 so we don't need to subtract by 1
-def valid_moves(game_dict):
+def valid_col(game_dict):
     grid = game_dict['grid']        # Save the 'grid' key from our dict read from the json obj
     moves = []  # Can't initialize w/ Int 64s
 
@@ -46,93 +54,35 @@ def valid_moves(game_dict):
 
     return moves
 
-def get_valid_locations(board):
+def valid_moves(grid, col, row):
+    return grid[row-1][col] == 0
+
+
+def get_valid_locations(board, row):
 	valid_locations = []
-	for col in range(COLUMN_COUNT):
-		if is_valid_location(board, col):
+	for col in range(row):
+		if valid_moves(board, col):
 			valid_locations.append(col)
 	return valid_locations
 
-def evaluate_window(window, piece):
-	score = 0
-	opp_piece = PLAYER_PIECE
-	if piece == PLAYER_PIECE:
-		opp_piece = AI_PIECE
 
-	if window.count(piece) == 4:
-		score += 100
-	elif window.count(piece) == 3 and window.count(EMPTY) == 1:
-		score += 5
-	elif window.count(piece) == 2 and window.count(EMPTY) == 2:
-		score += 2
-
-	if window.count(opp_piece) == 3 and window.count(EMPTY) == 1:
-		score -= 4
-
-	return score
-
-def score_position(board, piece, col, height):
-	score = 0
-
-	## Score center column
-	center_array = [int(i) for i in list(board[:, col//2])]
-	center_count = center_array.count(piece)
-	score += center_count * 3
-
-	## Score Horizontal
-	for r in range(height):
-		row_array = [int(i) for i in list(board[r,:])]
-		for c in range(col-3):
-			window = row_array[c:c+WINDOW_LENGTH]
-			score += evaluate_window(window, piece)
-
-	## Score Vertical
-	for c in range(col):
-		col_array = [int(i) for i in list(board[:,c])]
-		for r in range(height-3):
-			window = col_array[r:r+WINDOW_LENGTH]
-			score += evaluate_window(window, piece)
-
-	## Score posiive sloped diagonal
-	for r in range(height-3):
-		for c in range(col-3):
-			window = [board[r+i][c+i] for i in range(WINDOW_LENGTH)]
-			score += evaluate_window(window, piece)
-
-	for r in range(height-3):
-		for c in range(col-3):
-			window = [board[r+3-i][c+i] for i in range(WINDOW_LENGTH)]
-			score += evaluate_window(window, piece)
-
-	return score
-
-def minimax(grid, depth, alpha, beta, maximizingPlayer):
-	valid_locations = get_valid_locations(grid)
-	is_terminal = is_terminal_node(grid)
-	if depth == 0 or is_terminal:
-		if is_terminal:
-			if winning_move(grid, AI_PIECE):
-				return (None, 100000000000000)
-			elif winning_move(grid, PLAYER_PIECE):
-				return (None, -10000000000000)
-			else: # Game is over, no more valid moves
-				return (None, 0)
-		else: # Depth is zero
-			return (None, score_position(grid, AI_PIECE))
+def minimax(grid, width, depth, alpha, beta, maximizingPlayer):
+	valid_locations = get_valid_locations(grid, width)
+        
 	if maximizingPlayer:
-		value = -math.inf
+		maxValue = -math.inf
 		column = random.choice(valid_locations)
 		for col in valid_locations:
-			row = get_next_open_row(grid, col)
-			b_copy = grid.copy()
-			new_score = minimax(b_copy, depth-1, alpha, beta, False)[1]
-			if new_score > value:
-				value = new_score
+			row = find_next_row(grid, col)
+			grid_copy = grid.copy()
+			new_score = minimax(grid_copy, width, depth-1, alpha, beta, False)[1]
+			if new_score > maxValue:
+				maxValue = new_score
 				column = col
-			alpha = max(alpha, value)
-			if alpha >= beta:
+			alpha = max(alpha, maxValue)
+			if beta <= alpha:
 				break
-		return column, value
+		return column
     
 # def minimax(position, depth, alpha, beta, maximizingPlayer):
 #     if depth == 0 or game over in position:
@@ -157,7 +107,6 @@ def minimax(grid, depth, alpha, beta, maximizingPlayer):
 #                 break;
 #             return minEval
 
-
 def main():
     print("Connect Four in Python", file=sys.stderr)
     alpha = -math.inf
@@ -168,16 +117,23 @@ def main():
         print(line, file=sys.stderr)
         game_dict = json.loads(line)        # This loads the game as a dict: grid, height, player #, width
         grid = get_board(game_dict)
-        # col = get_width(game_dict)
         height = get_depth(game_dict)
-        moves = valid_moves(game_dict)      # Returns a list of all valid moves
+        width = get_width(game_dict)
 
         # We'll utilize mini-max & alpha-beta pruning to choose the next best move
         # Player 1(AI) will be seeking a max value vs Player 2 - min value
-        move = random.choice(moves) # Let's ignore this random move generator
-        
+        # col = minimax(grid, width, height, -math.inf, math.inf, 1)
+
+        # if valid_moves(grid, col, width):
+        #     row = find_next_row(grid, col, height)
+
+        # action = {'move': col}
+        # print("col:", col, file=sys.stderr)
+        # print("row:", col, file=sys.stderr)
 
         #row = get_next_open_row(grid, col, height)
+        moves = valid_col(game_dict)      # Returns a list of all valid moves
+        move = random.choice(moves) # Let's ignore this random move generator
         action = {'move': move}
 
         action_json = json.dumps(action)
@@ -193,7 +149,6 @@ def main():
                 row = get_next_open_row(board, col)
                 drop_piece(board, row, col, AI_PIECE)
         """
-        
 
 if __name__ == '__main__':
     main()
